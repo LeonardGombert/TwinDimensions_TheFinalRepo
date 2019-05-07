77cc898e6014efe85fee8137e5d85b90ba556c12
Léonard Gombert <epoc4oftime@gmail.com>
2019-05-07 18:31:38 +0200

    HOOK TOWER + NEW PREFABS

--- Twin Dimensions/Assets/Scripts/Leonard_Scripts/Environement/Portals.cs
+++ Twin Dimensions/Assets/Scripts/Leonard_Scripts/Environement/Portals.cs
@@ -6,17 +6,6 @@ using Sirenix.Serialization;
 
 public class Portals : MonoBehaviour
 {
-    public GameObject portalEntrance;
-    public GameObject portalExit;
-    bool isOnPortal = false;
-    GameObject player;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
-
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Player")
