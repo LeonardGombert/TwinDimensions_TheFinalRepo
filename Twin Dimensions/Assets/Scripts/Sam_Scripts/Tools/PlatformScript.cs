69330aa1f8b799e3f092fe234ea454439ed68af3
SamuelD <workflow.samuel@gmail.com>
2019-05-07 15:19:07 +0200

    LD//Fixed pressure plate collisions

---
+++ Twin Dimensions/Assets/Scripts/Sam_Scripts/Tools/PlatformScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    Vector2 positionA;
+    Vector2 positionB;
+
+    public Transform objectPositionA;
+    public Transform objectPositionB;
+
+    void Awake()
+    {
+        positionA = objectPositionA.position;
+        positionB = objectPositionB.position;
+
+        gameObject.transform.position = positionA;
+    }
+
+    void Activated()
+    {
+        gameObject.transform.position = positionB;
+    }
+
+    void Released()
+    {
+        gameObject.transform.position = positionA;
+    }
+}
