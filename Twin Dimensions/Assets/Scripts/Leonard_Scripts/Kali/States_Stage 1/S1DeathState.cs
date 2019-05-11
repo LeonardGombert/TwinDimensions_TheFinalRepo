3e044ea13a78c6f4d13a9e49c6250aad1ce0752d
Léonard Gombert <epoc4oftime@gmail.com>
2019-05-11 20:32:31 +0200

    KALI//Slam Attack 75%

--- Twin Dimensions/Assets/Scripts/Leonard_Scripts/Kali/States_Stage 1/S1DeathState.cs
+++ Twin Dimensions/Assets/Scripts/Leonard_Scripts/Kali/States_Stage 1/S1DeathState.cs
@@ -5,6 +5,10 @@ public class S1DeathState : State<KaliBossAI>
 {
     private static S1DeathState _instance;
 
+    BoxCollider2D activeAttackBoxCol2D;
+    BoxCollider2D rightAttackBoxCol2D;
+    BoxCollider2D leftAttackBoxCol2D;
+
     private S1DeathState()
     {
         if(_instance != null)
@@ -31,6 +35,12 @@ public class S1DeathState : State<KaliBossAI>
     public override void EnterState(KaliBossAI _owner)
     {
         Debug.Log("Entering Idle State");
+        activeAttackBoxCol2D = _owner.activeAttackBoxCol2D;
+        rightAttackBoxCol2D = _owner.rightAttackBoxCol2D;
+        leftAttackBoxCol2D = _owner.leftAttackBoxCol2D;
+        
+        rightAttackBoxCol2D.enabled = false;
+        leftAttackBoxCol2D.enabled = false;
     }
 
     public override void ExitState(KaliBossAI _owner)
---
+++ Twin Dimensions/Assets/Scripts/Leonard_Scripts/MonoScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoScript : MonoBehaviour
+{
+    void Start()
+    {
+        StateData nonMonoScript = new StateData();
+        //Pass MonoBehaviour to non MonoBehaviour class
+        nonMonoScript.monoParser(this);
+    }
+}
