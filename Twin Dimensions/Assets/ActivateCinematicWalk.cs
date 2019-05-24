10c01eb4285b58a31b72231133d6a6db0d288fac
Léonard Gombert <epoc4oftime@gmail.com>
2019-05-25 00:20:32 +0200

    CINEMATIC WALK UP

---
+++ Twin Dimensions/Assets/ActivateCinematicWalk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCinematicWalk : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            anim.SetBool("openDoor", true);
+            PlayerController.cinematicMoveUp = true;              
+        }
+    }
+}
--- Twin Dimensions/Assets/Scripts/Leonard_Scripts/KaliDoor.cs
+++ Twin Dimensions/Assets/Scripts/Leonard_Scripts/KaliDoor.cs
@@ -4,26 +4,13 @@ using UnityEngine;
 
 public class KaliDoor : MonoBehaviour
 {
-    Animator anim; 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.tag == "Player") 
+        if (collider.tag == "Player")
         {
-            anim.SetBool("openDoor", true);
-            PlayerController.cinematicMoveUp = true;
+            GameObject manager;
+            manager = GameObject.FindGameObjectWithTag("Manager");
+            manager.gameObject.SendMessage("ReachedExit");               
         }
     }
 }
